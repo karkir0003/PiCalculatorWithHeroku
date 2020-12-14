a9bab23e7de915d98e4db1b20bddee59ba1e290e
valistelea <vstelea3@gatech.edu>
2020-12-14 15:56:52 -0500

    Updating Flask

--- hello.py
+++ hello.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,5 +6,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/handle_data', methods=['POST'])
+def handle_data():
+    value = request.form['list']
+    print('hello')
+    print(value)
+
+
 if __name__ == '__main__':
-   app.run()
+    app.run()
